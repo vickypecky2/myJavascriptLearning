b9829be7785bb27788c1de748c99ebbd2f01e650
Vickypecky <vickypecky@gmail.com>
2020-05-14 18:20:42 -0700

    Initial Commit

---
+++ studentGrade.js
@@ -0,0 +1,33 @@
+// let = studentGrade
+// let number = 10;
+// console.log("we/'re learning about escaping characters!")
+ 
+// let studentGrade = studentNAme;
+// console.log('studentGrade');
+
+function gradeSystem(studentGrade) {
+   console.log( "get studentGrade");
+   
+    // let  avgmark= 0;
+
+    // for (let i = 0; i < studentGrade.length; i++) {
+    //     let avgmark += studentgrade;
+        
+    }
+
+    if (studentGrade < 100) {
+        console.log( "student");
+    } else {
+        
+    }
+
+    
+
+
+
+
+
+
+
+
+
