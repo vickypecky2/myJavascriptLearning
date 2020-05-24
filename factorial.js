137b8700e291bc229ea802e48e0be8bcb9b7309f
Vickypecky <vickypecky@gmail.com>
2020-05-24 16:59:45 -0700

    commit repeatedNumber and Factorial

---
+++ factorial.js
@@ -0,0 +1,9 @@
+// ********************************factorial calculation*************//
+function factorial(n) {
+    if (n > 0 && n <= 1) {
+        return 1;
+    }
+    else {
+        return n*factorial(n-1);
+    }
+}
---
+++ repeatNumber.js
@@ -0,0 +1,14 @@
+//***********************************repeat number from 1-100 */
+// using while loop
+while (num < 100) {
+    let num =+ 1;
+    console.log(num);
+}
+
+// using for loop
+for (let num = 0; num <= 100; num++) {
+    // const element = array[num];
+    console.log(num);
+}
+
+
