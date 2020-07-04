31839213f4043b2efa589e02cce95a7e287d8533
Lanre Lawal <larrystonegroup@gmail.com>
2020-07-04 16:53:57 +0100

    corrections to the existing problem solutions

--- repeatNumber.js
+++ repeatNumber.js
@@ -1,14 +1,25 @@
 //***********************************repeat number from 1-100 */
-// using while loop
+// using while 
+let num = 1;
 while (num < 100) {
-    let num =+ 1;
-    console.log(num);
+  console.log(++num);
+  // num += 1; // 2
+  // num++, ++num
 }
 
 // using for loop
 for (let num = 0; num <= 100; num++) {
-    // const element = array[num];
-    console.log(num);
+  // const element = array[num];
+  console.log(num);
 }
 
 
+console.log(num++) ==== {
+  console.log(num);
+  num += 1;
+}
+
+console.log(++num) ==== {
+  num += 1;
+  console.log(num);
+}
